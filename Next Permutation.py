ea4129e71894ac148e246f3d66083d9995d06926
Xue-Yuan <cqnkxy@gmail.com>
2016-09-22 13:52:57 -0700

    very nicely written

--- Next Permutation.py
+++ Next Permutation.py
@@ -4,16 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        k = -1
-        for i in range(len(nums)-1):
-            if nums[i] < nums[i+1]:
-                k = i
-        if k == -1:
-            nums[:] = nums[::-1]
-        else:
-            l = -1
-            for i in range(k+1, len(nums)):
-                if nums[k] < nums[i]:
-                    l = i
-            nums[k], nums[l] = nums[l], nums[k]
-            nums[:] = nums[:k+1] + nums[:k:-1]
+        sz = len(nums)
+        for i in reversed(range(sz)):
+            if i > 0 and nums[i-1] < nums[i]:
+                for j in reversed(range(sz)):
+                    if nums[i-1] < nums[j]:
+                        nums[i-1], nums[j] = nums[j], nums[i-1]
+                        break
+                break
+        nums[i:] = nums[i:][::-1]
