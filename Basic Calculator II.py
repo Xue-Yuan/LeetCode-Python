a76859b5011446121db1d4d8e8f88211d4c8ed37
Yuán <cqnkxy@gmail.com>
2024-06-17 16:11:27 -0700

    basic calculator II

--- Basic Calculator II.py
+++ Basic Calculator II.py
@@ -1,21 +1,62 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        from operator import mul, div, add, sub
-        m = {'+': add, '-': sub, '*': mul, '/': lambda x, y: x/y if x*y >= 0 else -(abs(x)/abs(y))}
-        nums, ops = [], []
-        num, op = '0', '+'
-        for ch in s+'+':
+from typing import List
+
+
+class Solution:
+    '''
+    3          +      2                *  2
+    pre_num    op   cur_num
+    '''
+
+    def calculate(self, s: str) -> int:
+        res = 0
+        pre_num = 0
+        cur_num = 0
+        op = '+'
+        for i, ch in enumerate(s):
             if ch.isdigit():
-                num += ch
-            elif ch != ' ':
-                if op in '+-':
-                    nums.append(m[op](0, int(num)))
-                else:
-                    nums[-1] = m[op](nums[-1], int(num))
-                num = '0'
+                cur_num = cur_num * 10 + int(ch)
+            if ch in '+-/*' or i == len(s) - 1:
+                if op == '+':
+                    res += pre_num
+                    pre_num = cur_num
+                elif op == '-':
+                    res += pre_num
+                    pre_num = -cur_num
+                elif op == "*":
+                    pre_num = pre_num * cur_num
+                elif op == '/':
+                    pre_num = int(pre_num / cur_num)
                 op = ch
-        return sum(nums)
+                cur_num = 0
+        res += pre_num
+        return res
+
+
+class Solution2:
+
+    def calculate(self, s: str) -> int:
+        stk = []
+        num = 0
+        sign = "+"
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == " ":
+                continue
+            elif ch in "+-*/":
+                self.update(stk, sign, num)
+                sign = ch
+                num = 0
+        self.update(stk, sign, num)
+        return sum(stk)
+
+    def update(self, stk: List[int], op: str, num: int) -> None:
+        if op == "+":
+            stk.append(num)
+        elif op == "-":
+            stk.append(-num)
+        elif op == "/":
+            stk[-1] = int(stk[-1] / num)
+        elif op == "*":
+            stk[-1] = stk[-1] * num
