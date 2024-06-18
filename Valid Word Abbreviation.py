b21e919c695e01729e50f9fcec563d95f8f0b2aa
Yuán <cqnkxy@gmail.com>
2024-06-18 12:02:08 -0700

    Valid Word Abbrevation

--- Valid Word Abbreviation.py
+++ Valid Word Abbreviation.py
@@ -1,19 +1,32 @@
-class Solution(object):
-    def validWordAbbreviation(self, word, abbr):
-        """
-        :type word: str
-        :type abbr: str
-        :rtype: bool
-        """
-        idx, num, sz = 0, 0, len(word)
+class Solution:
+
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        sz = len(word)
+        num = 0
+        idx = 0
         for ch in abbr:
+            if num == 0 and ch == '0':
+                # No leading 0s
+                return False
             if ch.isdigit():
-                if num == 0 and ch == '0':
-                    break
-                num = num*10 + int(ch)
+                num = num * 10 + int(ch)
             else:
-                idx, num = idx+num, 0
-                if not ((idx < sz and word[idx] == ch) or idx == sz):
-                    break
+                idx += num
+                num = 0
+                if idx >= sz or word[idx] != ch:
+                    return False
                 idx += 1
-        return (idx+num) == sz
+        return idx + num == sz
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.validWordAbbreviation('substitution', 's10n'))
+    print(s.validWordAbbreviation('substitution', 'sub4u4'))
+    print(s.validWordAbbreviation('substitution', '12'))
+    print(s.validWordAbbreviation('substitution', 'su3i1u2on'))
+    print(s.validWordAbbreviation('substitution', 'substitution'))
+    print('------------------------------')
+    print(s.validWordAbbreviation('substitution', 's55n'))
+    print(s.validWordAbbreviation('substitution', 's010n'))
+    print(s.validWordAbbreviation('substitution', 's0ubstitution'))
