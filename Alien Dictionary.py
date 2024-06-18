c6b479adb3f2681e680b81a4596ed926e2fb6808
Yuán <cqnkxy@gmail.com>
2024-06-18 11:28:08 -0700

    Alien Dictionary

--- Alien Dictionary.py
+++ Alien Dictionary.py
@@ -2,11 +2,14 @@
 prefix of the previous word, then there is no valid order.
 """
 
+from typing import List
 import collections
+import itertools
 
 
-class Solution(object):
-    def alienOrder(self, words):
+class Solution:
+
+    def alienOrder(self, words: List[str]):
         """
         :type words: List[str]
         :rtype: str
@@ -14,16 +17,17 @@ class Solution(object):
         charset = set(''.join(words))
         indegrees = {x: 0 for x in charset}
         graph = {x: [] for x in charset}
-        for pair in zip(words, words[1:]):
-            for ch1, ch2 in zip(*pair):
+        for w1, w2 in itertools.pairwise(words):
+            for ch1, ch2 in zip(w1, w2):
                 if ch1 != ch2:
                     graph[ch1].append(ch2)
                     indegrees[ch2] += 1
                     break
             else:
-                if len(pair[0]) > len(pair[1]):
+                if len(w1) > len(w2):
                     return ''
-        q = collections.deque(k for k, v in indegrees.items() if v == 0)
+
+        q = collections.deque(k for k in charset if indegrees[k] == 0)
         ans = ''
         while q:
             cur = q.popleft()
@@ -37,7 +41,8 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-    solution = Solution()
-    print solution.alienOrder(["wrt", "wrf", "er", "ett", "rftt"])
-    print solution.alienOrder(['ak', 'bj'])
-    print solution.alienOrder(["ab", "adc"])
+    s = Solution()
+    print(s.alienOrder(["wrt", "wrf", "er", "ett", "rftt"]))
+    print(s.alienOrder(['ak', 'bj']))
+    print(s.alienOrder(["ab", "adc"]))
+    print(s.alienOrder(["x", "z", "x"]))
