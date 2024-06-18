e26e7b3ec73db856f3f89df46cb754fa485b25a6
Yuán <cqnkxy@gmail.com>
2024-06-18 16:28:14 -0700

    Kth Largest Element in an Array

--- Kth Largest Element in an Array.py
+++ Kth Largest Element in an Array.py
@@ -1,75 +1,93 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        def quickselect(nums, b, e, k):
-            if b > e:
-                return
-            i, j = b-1, e
-            while i < j:
-                i, j = i+1, j-1
-                while nums[j] > nums[e]:
-                    j -= 1
-                while nums[i] < nums[e]:
-                    i += 1
-                if i < j:
-                    nums[i], nums[j] = nums[j], nums[i]
-            if nums[i] > nums[e]:
-                nums[i], nums[e] = nums[e], nums[i]
-            if k-1 == i:
-                return nums[i]
-            elif k-1 > i:
-                return quickselect(nums, i+1, e, k)
-            else:
-                return quickselect(nums, b, i-1, k)
-
-        return quickselect(nums, 0, len(nums)-1, len(nums)-k+1)
+from typing import List, Tuple
+import random
 
 
-# A huge difference with partition
-class Solution2(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        def median3(nums, b, e):
-            m = (b+e) >> 1
-            if nums[b] > nums[e]:
-                nums[b], nums[e] = nums[e], nums[b]
-            if nums[b] > nums[m]:
-                nums[b], nums[m] = nums[m], nums[b]
-            if nums[m] < nums[e]:
-                nums[m], nums[e] = nums[e], nums[m]
-            return nums[e]
+class Solution:
 
-        def quickselect(nums, b, e, k):
-            if b >= e:
-                return nums[k-1]
-            if b+1 == e:
-                if nums[b] > nums[e]:
-                    nums[b], nums[e] = nums[e], nums[b]
-                return nums[k-1]
-            pivot = median3(nums, b, e)
-            i, j = b, e
-            while i < j:
-                i, j = i+1, j-1
-                while nums[j] > pivot:
-                    j -= 1
-                while nums[i] < pivot:
-                    i += 1
-                if i < j:
-                    nums[i], nums[j] = nums[j], nums[i]
-            nums[i], nums[e] = nums[e], nums[i]
-            if k-1 == i:
-                return nums[i]
-            elif k-1 > i:
-                return quickselect(nums, i+1, e, k)
-            else:
-                return quickselect(nums, b, i-1, k)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        target = len(nums) - k
+        return self.quickSelect(nums, 0, len(nums) - 1, target)
 
-        return quickselect(nums, 0, len(nums)-1, len(nums)-k+1)
+    def quickSelect(self, nums: List[int], low: int, high: int,
+                    target: int) -> int:
+        b, e = self.partition(nums, low, high)
+        if b <= target <= e:
+            return nums[target]
+        if e < target:
+            return self.quickSelect(nums, e + 1, high, target)
+        else:
+            return self.quickSelect(nums, low, b - 1, target)
+
+    def partition(self, items: List[int], low: int,
+                  high: int) -> Tuple[int, int]:
+        # Choose a random pivot to improve performance
+        p = random.randint(low, high)
+        items[p], items[high] = items[high], items[p]
+        pivot = items[high]
+        i = low
+
+        for j in range(low, high):
+            if items[j] < pivot:
+                items[i], items[j] = items[j], items[i]
+                i += 1
+
+        # Move values equal to pivot together in the middle. Otherwise
+        # fail the last test case where a huge amount of same values packed
+        # in the middle.
+        start = -1
+        for j in range(i, high):
+            if items[j] == pivot:
+                start = start if start >= 0 else j
+                items[i], items[j] = items[j], items[i]
+                i += 1
+        items[i], items[high] = items[high], items[i]
+        return (start, i) if start >= 0 else (i, i)
+
+
+class Solution2:
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Same idea, but uses more space
+        pivot = random.choice(nums)
+        left = [x for x in nums if x > pivot]
+        middle = [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+        if len(left) + len(middle) >= k > len(left):
+            return middle[0]
+        if k <= len(left):
+            return self.findKthLargest(left, k)
+        return self.findKthLargest(right, k - len(left) - len(middle))
+
+
+class Solution3:
+    '''Basic version of solution1 without optimization. Fails the online judge.
+    '''
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        target = len(nums) - k
+        return self.quickSelect(nums, 0, len(nums) - 1, target)
+
+    def quickSelect(self, nums: List[int], low: int, high: int,
+                    target: int) -> int:
+        pivot = self.partition(nums, low, high)
+        if target == pivot:
+            return nums[target]
+        if pivot < target:
+            return self.quickSelect(nums, pivot + 1, high, target)
+        else:
+            return self.quickSelect(nums, low, pivot - 1, target)
+
+    def partition(self, items: List[int], low: int, high: int) -> int:
+        # Choose a random pivot to improve performance
+        p = random.randint(low, high)
+        items[p], items[high] = items[high], items[p]
+        pivot = items[high]
+        i = low
+
+        for j in range(low, high):
+            if items[j] <= pivot:
+                items[i], items[j] = items[j], items[i]
+                i += 1
+        items[i], items[high] = items[high], items[i]
+
+        return i
