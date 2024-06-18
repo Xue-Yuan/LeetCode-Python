480f53b5939a13b1dd5e47a7c05f976a82e71491
Yuán <cqnkxy@gmail.com>
2024-06-17 23:56:31 -0700

    Remove invalid parentheses

--- Remove Invalid Parentheses.py
+++ Remove Invalid Parentheses.py
@@ -9,7 +9,7 @@ class Solution(object):
         :rtype: List[str]
         """
 
-        def is_valid(s):
+        def isValid(s):
             cnt = 0
             for ch in s:
                 cnt += ch == '('
@@ -20,58 +20,18 @@ class Solution(object):
 
         lvl = {s}
         while lvl:
-            valid = filter(is_valid, lvl)
+            valid = list(filter(isValid, lvl))
             if valid:
                 return valid
             lvl = {
                 s[:i] + s[i + 1:]
                 for s in lvl
-                for i in range(len(s)) if i == 0 or s[i] != s[i - 1]
+                for i in range(len(s)) if s[i] in '()'
             }
         return []
 
 
-class Solution2(object):
-
-    def removeInvalidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        left = right = 0
-        for ch in s:
-            left += ch == '('
-            if left == 0:
-                right += ch == ')'
-            else:
-                left -= ch == ')'
-
-        def dfs(s, beg, left, right, ans=[]):
-            if left == right == 0:
-                if self.is_valid(s):
-                    ans.append(s)
-                return ans
-            for i in range(beg, len(s)):
-                if i == 0 or s[i] != s[i - 1]:
-                    if left > 0 and s[i] == '(':
-                        dfs(s[:i] + s[i + 1:], i, left - 1, right)
-                    if right > 0 and s[i] == ')':
-                        dfs(s[:i] + s[i + 1:], i, left, right - 1)
-            return ans
-
-        return dfs(s, 0, left, right)
-
-    def is_valid(self, s):
-        cnt = 0
-        for ch in s:
-            cnt += ch == '('
-            cnt -= ch == ')'
-            if cnt < 0:
-                return False
-        return cnt == 0
-
-
-class Solution3:
+class Solution2:
 
     def removeInvalidParentheses(self, s: str) -> List[str]:
         ans = []
@@ -87,8 +47,8 @@ class Solution3:
                 if open_cnt >= close_cnt:
                     continue
                 for j in range(last_removed_idx, i + 1):
-                    if s[j] == close and (j == last_removed_idx
-                                          or s[j] != s[j - 1]):
+                    if (s[j] == close
+                            and (j == last_removed_idx or s[j] != s[j - 1])):
                         remove(s[:j] + s[j + 1:], i, j, open, close)
                 return
             r = s[::-1]
