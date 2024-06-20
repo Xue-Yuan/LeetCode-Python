de3a02a222c7178e9c022cd55e14b7e9344d8a9f
Yuán <cqnkxy@gmail.com>
2024-06-19 20:08:18 -0700

    Minimum Window Substring

--- Minimum Window Substring.py
+++ Minimum Window Substring.py
@@ -1,22 +1,52 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+import collections
+
+
+class Solution:
+
+    def minWindow(self, s: str, t: str) -> str:
         need = collections.Counter(t)
         found = collections.Counter()
         b, cnt, ans = 0, 0, ''
         for idx, ch in enumerate(s):
-            if ch in need:
+            found[ch] += 1
+            cnt += found[ch] <= need[ch]
+            while cnt == len(t):
+                if not ans or len(ans) > idx - b + 1:
+                    ans = s[b:idx + 1]
+                cnt -= found[s[b]] <= need[s[b]]
+                found[s[b]] -= 1
+                b += 1
+        return ans
+
+
+class Solution2:
+    '''Two pointers; sliding window.
+    '''
+
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        found = collections.Counter()
+        sz_t = len(t)
+        sz_s = len(s)
+        total = 0
+        l = h = 0
+        ans = ''
+        while h < sz_s:
+            while h < sz_s and total < sz_t:
+                # Move high pointer to find a substring that contains all
+                # characters in t
+                ch = s[h]
                 found[ch] += 1
-                cnt += found[ch] <= need[ch]
-                while cnt == len(t):
-                    if not ans or len(ans) > idx-b+1:
-                        ans = s[b:idx+1]
-                    if s[b] in need:
-                        cnt -= found[s[b]] <= need[s[b]]
-                        found[s[b]] -= 1
-                    b += 1
+                if found[ch] <= need[ch]:
+                    total += 1
+                h += 1
+            while l < sz_s and total == sz_t:
+                # Move low pointer to make the substring smaller.
+                if ans == '' or len(ans) > h - l:
+                    ans = s[l:h]
+                ch = s[l]
+                found[ch] -= 1
+                if found[ch] < need[ch]:
+                    total -= 1
+                l += 1
         return ans
