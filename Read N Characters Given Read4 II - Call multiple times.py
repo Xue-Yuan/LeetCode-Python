b7939939bec8d9ff78ea9548fee62c9c05f5ab29
Yuán <cqnkxy@gmail.com>
2024-07-11 11:34:35 -0700

    Read N Characters Given Read4 II

--- Read N Characters Given Read4 II - Call multiple times.py
+++ Read N Characters Given Read4 II - Call multiple times.py
@@ -1,15 +1,27 @@
-# The read4 API is already defined for you.
-# @param buf, a list of characters
-# @return an integer
-# def read4(buf):
+from typing import List
 
-class Solution(object):
+cnt = 0
+
+
+def read4(buf: List[str]) -> int:
+    global cnt
+    cnt += 1
+    buf[:] = "abcd"
+    if cnt == 5:
+        return 2
+    if cnt > 5:
+        return 0
+    return 4
+
+
+class Solution:
+
     def __init__(self):
-        self.save = ['']*4
+        self.save = [''] * 4
         self.end = 0
         self.start = 0
 
-    def read(self, buf, n):
+    def read(self, buf: List[str], n: int) -> int:
         """
         :type buf: Destination buffer (List[str])
         :type n: Maximum number of characters to read (int)
@@ -28,3 +40,47 @@ class Solution(object):
             if self.start == self.end:
                 self.start = 0
         return idx
+
+
+class Solution2:
+
+    def __init__(self):
+        self.save = []
+
+    def read(self, buf: List[str], n: int) -> int:
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        idx = 0
+        while idx < n:
+            if not self.save:
+                self.save = [''] * 4
+                num = read4(self.save)
+                self.save = self.save[:num]
+            if not self.save:
+                return idx
+            buf[idx] = self.save.pop(0)
+            idx += 1
+        return idx
+
+
+if __name__ == "__main__":
+    s = Solution()
+    cnt = 0
+    b1 = [''] * 10
+    print(s.read(b1, 10))
+    print(b1)
+    b1 = [''] * 14
+    print(s.read(b1, 14))
+    print(b1)
+
+    s2 = Solution2()
+    cnt = 0
+    b2 = [''] * 10
+    print(s2.read(b2, 10))
+    print(b2)
+    b2 = [''] * 14
+    print(s2.read(b2, 14))
+    print(b2)
