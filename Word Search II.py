e561a8156822cca7bcbeaf245cd6d675c618b115
Yuán <cqnkxy@gmail.com>
2024-07-29 15:29:07 -0700

    Word Search

--- Word Search II.py
+++ Word Search II.py
@@ -1,39 +1,45 @@
-class Solution(object):
-    def findWords(self, board, words):
-        """
-        :type board: List[List[str]]
-        :type words: List[str]
-        :rtype: List[str]
-        """
+from typing import List
+
+
+class Node:
+
+    def __init__(self):
+        self.m = {}
+        self.is_word = False
+
+
+class Solution:
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         if not any(board):
             return []
-        root = {}
+        root = Node()
         for word in words:
             node = root
             for ch in word:
-                if ch not in node:
-                    node[ch] = {}
-                node = node[ch]
-            node['\0'] = '\0'
+                if ch not in node.m:
+                    node.m[ch] = Node()
+                node = node.m[ch]
+            node.is_word = True
         row, col = len(board), len(board[0])
         ans = []
 
         def _dfs(r, c, node, path):
             if 0 <= r < row and 0 <= c < col:
                 cur = board[r][c]
-                if not cur or cur not in node:
+                if not cur or cur not in node.m:
                     return
                 save, board[r][c] = board[r][c], ''
                 path += save
-                child = node[cur]
-                if '\0' in child:
+                child = node.m[cur]
+                if child.is_word:
                     ans.append(path)
                     #avoid duplicates
-                    del child['\0']
-                _dfs(r+1, c, child, path)
-                _dfs(r-1, c, child, path)
-                _dfs(r, c+1, child, path)
-                _dfs(r, c-1, child, path)
+                    child.is_word = False
+                _dfs(r + 1, c, child, path)
+                _dfs(r - 1, c, child, path)
+                _dfs(r, c + 1, child, path)
+                _dfs(r, c - 1, child, path)
                 board[r][c] = save
 
         for r in range(row):
@@ -50,4 +56,5 @@ if __name__ == '__main__':
         ['i', 'f', 'l', 'v'],
     ]
     words = ["oath", "pea", "eat", "rain"]
-    print Solution().findWords(board, words)
+    s = Solution()
+    print(s.findWords(board, words))
